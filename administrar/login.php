424c7c15a5bdc237894e31d39b58e9d7c94f2016
gustavo-graos-santos <grgs95859@gmail.com>
2021-04-12 18:43:08 -0500

    ordenar carpetas

--- administrar/login.php
+++ administrar/login.php
@@ -7,67 +7,63 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="shortcut icon" href="img/favicon.ico" />
+    <link rel="shortcut icon" href="images/icons/favicon.ico" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!--===============================================================================================-->  
    <!--  <link rel="icon" type="image/png" href="images/icons/favicon.ico"/> -->
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="../fonts/iconic/css/material-design-iconic-font.min.css">
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
   <!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/animsition/css/animsition.min.css">
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
   <!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/daterangepicker/daterangepicker.css">
   <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/util.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
   <!--===============================================================================================-->
-    <title>Datos Incorrectos</title>
+    <title>Login Administrador</title>
   </head>
   <body>
-    <div class="container-login100" style="background-image: url('img/fondo.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center;">
+    <div class="container-login100" style="background-image: url('../img/fondo.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center;">
     <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-      <form class="login100-form validate-form">
-      	<div class="flex-c p-b-5">
-          <img src="images/icons/favicon.ico" alt="">
+      <form id="registrar" action="adminloguear.php" method="POST" class="login100-form validate-form">
+        <div class="flex-c p-b-5">
+          <img src="img/favicon.ico" alt="">
         </div>
-      	<br>
-        <div class="alert alert-danger" align="center">
-        	<?php  
-			require '../conexion.php';
-			session_start();
+        <span class="login100-form-title p-b-37">
+          Administrador
+        </span>
 
-			$usuarioad = $_POST['usuarioad'];
-			$clavead = $_POST['contrasenaad'];
+        <div class="wrap-input100 validate-input m-b-20" data-validate="Ingrese correo electrónico">
+          <input class="input100" type="text" name="usuarioad" placeholder="Correo Electrónico">
+          <span class="focus-input100"></span>
+        </div>
 
-			$q = "SELECT COUNT(*) FROM usuario where email = '$usuarioad' and password = '$clavead' and login = 1";
-			$consulta = mysqli_query($conexion,$q);
-			$array = mysqli_fetch_array($consulta);
+        <div class="wrap-input100 validate-input m-b-25" data-validate = "Ingrese Contraseña">
+          <input class="input100" type="password" name="contrasenaad" placeholder="Contraseña">
+          <span class="focus-input100"></span>
+        </div>
 
-			if($array['contar'>0]){
-				$_SESSION['username'] = $usuarioad;
-				header("location: index.php");
-			}else{
-				echo "Usted no es Administrador";
-			}
-
-			?>
-			</div>
+        <div class="container-login100-form-btn">
+          <button class="login100-form-btn">
+            Iniciar Sesión
+          </button>
+        </div>
       </form>
-      <br>
        <div class="text-center">
-          <a href="../login.php" class="txt2 hov1">
-            Volver
+          <a href="login.php" class="txt2 hov1">
+            Volver a Login
           </a>
         </div>
       
@@ -79,21 +75,22 @@
   <div id="dropDownSelect1"></div>
   
 <!--===============================================================================================-->
-  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/animsition/js/animsition.min.js"></script>
+  <script src="../vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/bootstrap/js/popper.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../vendor/bootstrap/js/popper.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/select2/select2.min.js"></script>
+  <script src="../vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/daterangepicker/moment.min.js"></script>
-  <script src="vendor/daterangepicker/daterangepicker.js"></script>
+  <script src="../vendor/daterangepicker/moment.min.js"></script>
+  <script src="../vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/countdowntime/countdowntime.js"></script>
+  <script src="../vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-  <script src="js/main.js"></script>
+  <script src="../js/main.js"></script>
+  <script type="text/javascript" src="../js/app.js"></script>
 
 
     <!-- Optional JavaScript; choose one of the two! -->
