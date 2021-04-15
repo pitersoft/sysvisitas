ed0fce81c94f86aa71fb59e14b8b323334df7c40
gustavo-graos-santos <grgs95859@gmail.com>
2021-04-14 19:41:52 -0500

    recuperar contraseña

--- enviarcorreo.php
+++ enviarcorreo.php
@@ -28,8 +28,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'grgs95859@gmail.com';                     //SMTP username
-    $mail->Password   = 'parkourparkour';                               //SMTP password
+    $mail->Username   = 'correopruebadesarrollo464@gmail.com';                     //SMTP username
+    $mail->Password   = 'Correoprueba464';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
